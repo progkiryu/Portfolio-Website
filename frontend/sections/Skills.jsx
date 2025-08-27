bbb9f2eca8267e444da2dc717e9a39af1b8f3b46
progkiryu <denmesa24@outlook.com>
2025-08-28 01:15:15 +1000

    change title of website

--- frontend/sections/Skills.jsx
+++ frontend/sections/Skills.jsx
@@ -2,7 +2,7 @@ function Skills() {
     return <div>
         <h1>Skills</h1>
 
-        <p>Primary Languages: C++, JavaScript/TypeScript, VanillaCSS, HTML5, Python</p>
+        <p>Primary Languages: C++, JavaScript, TypeScript, VanillaCSS, HTML5, Python</p>
         <p>Secondary Languages: C#, C, Java, PHP</p>
         <p>Frameworks: ReactJS, ElectronJS, NodeJS, ExpressJS</p>
         <p>Databases: MySQL, MongoDB</p>
