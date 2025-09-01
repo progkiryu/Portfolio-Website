5d59ad65caa814e9ab896db88df1160e6f3396e9
progkiryu <denmesa24@outlook.com>
2025-09-01 23:30:52 +1000

    add images to contact icons

--- frontend/sections/Contact.jsx
+++ frontend/sections/Contact.jsx
@@ -15,9 +15,13 @@ function Contact() {
         </div>
         <div className="h-[85vh] flex">
             <Email />
-            <div className="w-1/2">        
-                <a href="https://www.linkedin.com/in/denver-klein-mesa-aa323231a" target="_blank">LinkedIn</a>
-                <a href="https://github.com/progkiryu" target="_blank">GitHub</a>
+            <div className="w-1/2 flex items-center justify-center space-x-5">        
+                <a className="w-20" href="https://www.linkedin.com/in/denver-klein-mesa-aa323231a" title="LinkedIn" target="_blank">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/c/ca/LinkedIn_logo_initials.png" />
+                </a>
+                <a className="w-20" href="https://github.com/progkiryu" title="GitHub" target="_blank">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg" />
+                </a>
             </div>
         </div>
     </div>
