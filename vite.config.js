b014ce20e6d2c3e81ad20a7515a791af9b882b55
progkiryu <denmesa24@outlook.com>
2026-05-21 10:50:08 +1000

    add more changes for github pages

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/Portfolio-Website/'
+  base: '/Portfolio-Website/',
+  build: {
+    outDir: 'dist'
+  }
 })
